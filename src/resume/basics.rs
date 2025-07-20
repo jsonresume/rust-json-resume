015c6aaf89d9d1f45822516bf76089b67550c4f4
Alexis Besson <alexis@besson.tech>
2025-07-20 10:27:09 +0200

    add validation

--- src/resume/basics.rs
+++ src/resume/basics.rs
@@ -1,4 +1,6 @@
+use crate::validation::{validate_email, validate_url};
 use serde::{Deserialize, Serialize};
+use serde_valid::Validate;
 
 mod location;
 mod profile;
@@ -6,13 +8,16 @@ mod profile;
 use location::Location;
 use profile::Profile;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Validate)]
 pub struct Basics {
     pub name: Option<String>,
     pub label: Option<String>,
+    #[validate(custom = validate_url)]
     pub image: Option<String>,
+    #[validate(custom = validate_email)]
     pub email: Option<String>,
     pub phone: Option<String>,
+    #[validate(custom = validate_url)]
     pub url: Option<String>,
     pub summary: Option<String>,
     pub location: Option<Location>,
